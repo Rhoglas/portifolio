b2850144bc3e528c2d48ffebae549c72e9ea7cf2
Rhoglas Lobato Aragão <96073350+RhoglasLobato@users.noreply.github.com>
2022-08-05 16:06:20 -0300

    Portifólio-1.0
    Novas atualizações no meu portifólio para proporcionar experiências mais interessantes!!

--- scriptlist.js
+++ scriptlist.js
@@ -2,42 +2,22 @@ let techs = [
 	{
 		bg: "./img/bg/card1.svg",
 		img: "./img/icons/html.png",
-		txt: "HTML é uma linguagem de marcação de hiper texto utilizada na construção de páginas e documentos Web.",
-		progValue: "90",
-		progMax: "100",
-		porc: "90%",
 	},
 	{
 		bg: "./img/bg/card2.svg",
 		img: "./img/icons/css.png",
-		txt: "Cascading Style Sheets é um mecanismo para adicionar qualquer tipo de estilização de páginas e documentos web.",
-		progValue: "80",
-		progMax: "100",
-		porc: "80%",
 	},
 	{
 		bg: "./img/bg/card3.svg",
 		img: "./img/icons/js.png",
-		txt: "É uma linguagem de programação que permite a criação de conteúdos dinâmicos para deixar seu site mais interativo.",
-		progValue: "60",
-		progMax: "100",
-		porc: "60%",
 	},
 	{
 		bg: "./img/bg/card4.svg",
 		img: "./img/icons/git.png",
-		txt: "É um software livre e muito utilizado no desenvolvimento onde diversas pessoas podem criar e editar arquivos simultaneamente",
-		progValue: "70",
-		progMax: "100",
-		porc: "70%",
 	},
 	{
 		bg: "./img/bg/card5.svg",
 		img: "./img/icons/github.png",
-		txt: "É como se fosse uma rede social, só que de códigos, onde seus desenvolvedores podem disponibilizá-los para outras pessoas verem.",
-		progValue: "70",
-		progMax: "100",
-		porc: "70%",
 	},
 ];
 
